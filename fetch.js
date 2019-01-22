af892bd50ab8d617ba92bdd4bd7c06e37611fb67
unknown <5z1punch>
2019-01-22 15:39:18 +0800

    wqekasd

--- fetch.js
+++ fetch.js
@@ -2,5 +2,4 @@ export function fetchPolyfill(input, init) {
   return this.fetch(input,init);
 }
 
-fetchPolyfill.polyfill = true;
-export default = {"fetchPolyfill": fetchPolyfill};
+export default  {fetchPolyfill: fetchPolyfill};
